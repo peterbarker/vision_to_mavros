d6b9165203698837d3e266d70cd67c0ec8f71d1a
Peter Barker <pbarker@barker.dropbear.id.au>
2021-02-17 15:11:07 +1100

    d4xx_to_mavlink.py: flake8 fix

--- scripts/d4xx_to_mavlink.py
+++ scripts/d4xx_to_mavlink.py
@@ -1283,7 +1283,7 @@ class D4XXToMAVLink(object):
             s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
             s.connect(('8.8.8.8', 1))  # connect() for UDP doesn't send packets
             local_ip_address = s.getsockname()[0]
-        except Exception as e:
+        except Exception:
             local_ip_address = socket.gethostbyname(socket.gethostname())
         return local_ip_address
 
